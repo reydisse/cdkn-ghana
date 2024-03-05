bcd7fb1fe2abadb38af0a72cbc9ea66a9fa982cb
Rey Disse <reydisse@gmail.com>
2024-03-05 20:27:25 +0000

    bold

--- src/assets/css/style.js
+++ src/assets/css/style.js
@@ -246,7 +246,7 @@ export const GlobalCSS = () => {
                     font-family: "Myriad Pro", Helvetica, sans-serif;
                     line-height: ${theme.lineHeights.heading};
                     color: ${theme.colors.heading};
-                    // font-weight: ${theme.fontWeights.heading};
+                    font-weight: ${theme.fontWeights.heading};
                     margin: 0;
                     margin-bottom: 8px;
                 }
